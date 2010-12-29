18567eeead6039d4ad0b146609e48a92db0f8344
radek.varbuchta <radek.varbuchta@0d487e7e-b4c9-11dd-b6ab-49f5beb571ba>
2010-12-29 15:02:02 +0000

    remove unnecessary method

--- src/net/hextris/HextrisApplet.java
+++ src/net/hextris/HextrisApplet.java
@@ -4,68 +4,62 @@ import javax.swing.JApplet;
 import java.awt.event.KeyEvent;
 
 /**
- * Starting point for use of hextris as an applet.
- * This class simply loads the Hextris panel class
- * as it's content pane
+ * Starting point for use of hextris as an applet. This class simply loads the
+ * Hextris panel class as it's content pane
  * 
  * @author fränk
  * @author Radek Varbuchta
  */
 public class HextrisApplet extends JApplet {
 
-    private static final long serialVersionUID = -8291800152310127713L;
-    private Hextris hextris = null;
+	private static final long serialVersionUID = -8291800152310127713L;
+	private Hextris hextris = null;
 
-    public HextrisApplet() {
-        super();
-    }
+	public HextrisApplet() {
+		super();
+	}
 
-    @Override
-    public void init() {
-        hextris = getHextris();
-        setContentPane(hextris);
-        setSize(hextris.getPreferredSize());
+	@Override
+	public void init() {
+		hextris = getHextris();
+		setContentPane(hextris);
+		setSize(hextris.getPreferredSize());
 
-        hextris.addKeyListener(new java.awt.event.KeyAdapter() {
+		hextris.addKeyListener(new java.awt.event.KeyAdapter() {
 
-            @Override
-            public void keyPressed(java.awt.event.KeyEvent e) {
-                gameKeyPressed(e);
-            }
-        });
+			@Override
+			public void keyPressed(java.awt.event.KeyEvent e) {
+				gameKeyPressed(e);
+			}
+		});
 
-    }
+	}
 
-    @Override
-    public void start() {
-        super.start();
-    }
+	@Override
+	public void stop() {
+		super.stop();
+		hextris.gameOver();
+	}
 
-    @Override
-    public void stop() {
-        super.stop();
-        hextris.gameOver();
-    }
+	private Hextris getHextris() {
+		if (hextris == null) {
+			hextris = new Hextris(Hextris.APPLET);
+			hextris.setFocusable(true);
+			hextris.setRequestFocusEnabled(true);
+		}
+		return hextris;
+	}
 
-    private Hextris getHextris() {
-        if (hextris == null) {
-            hextris = new Hextris(Hextris.APPLET);
-            hextris.setFocusable(true);
-            hextris.setRequestFocusEnabled(true);
-        }
-        return hextris;
-    }
+	private void gameKeyPressed(KeyEvent e) {
+		int kc = e.getKeyCode();
+		int km = e.getModifiers();
 
-    private void gameKeyPressed(KeyEvent e) {
-        int kc = e.getKeyCode();
-        int km = e.getModifiers();
-
-        if (kc == KeyEvent.VK_N && (km & KeyEvent.ALT_MASK) != 0) {
-            getHextris().newGame(false, true);
-        } else if (kc == KeyEvent.VK_D && (km & KeyEvent.ALT_MASK) != 0) {
-            getHextris().newGame(true, true);
-        } else {
-            getHextris().gameKeyPressed(e);
-        }
-    }
+		if (kc == KeyEvent.VK_N && (km & KeyEvent.ALT_MASK) != 0) {
+			getHextris().newGame(false, true);
+		} else if (kc == KeyEvent.VK_D && (km & KeyEvent.ALT_MASK) != 0) {
+			getHextris().newGame(true, true);
+		} else {
+			getHextris().gameKeyPressed(e);
+		}
+	}
 }
